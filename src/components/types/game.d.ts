8791514213b025ee244ee0280d279db3d22ff0f3
Libro <libro.1115@gmail.com>
2025-06-27 15:53:40 +0900

    構造体修正

--- src/components/types/game.d.ts
+++ src/components/types/game.d.ts
@@ -1,20 +1,20 @@
 // game.d.ts
 export type PieceColor = 'red' | 'blue' | 'green' | 'yellow'; // △と▽の色
-export type PieceType = 'triangle' | 'invertedTriangle' | 'empty'; // ピースの種類 (△, ▽, 空)
+export type PieceType = 'triangle' | 'floatInvertedTriangle' | `intertedTriangle` | 'empty'; // ピースの種類 (△, 浮いた▽,▽, 空)
 
 export interface Piece {
   type: PieceType;
   color: PieceColor;
-  // 他にも、落下中のピースなら 'x', 'y' 座標など
+  row:number;
+  column:number;
+  subPixcel:number;
 }
 
 export type BoardState = Piece[][]; // 盤面はピースの2次元配列
 
 export interface GameState {
   board: BoardState;
-  fallingPiece: Piece;
-  fallingPiecePosition: { x: number; y: number; };
-  nextPiece: Piece;
+  PiecePosition: Array<Array<Piece>>;
   score: number;
   isGameOver: boolean;
   isPaused: boolean;
